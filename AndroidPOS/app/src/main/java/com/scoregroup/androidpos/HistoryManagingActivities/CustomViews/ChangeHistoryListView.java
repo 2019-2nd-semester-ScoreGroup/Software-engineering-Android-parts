05cbb238e6aca6cf02cf64f1cd101eb528576a67
정상우 <jsu4838@naver.com>
2019-11-03 16:17:32 +0900

    뷰용 레이아웃 골조 구현
    비주얼 고려는 나중에

--- AndroidPOS/app/src/main/java/com/scoregroup/androidpos/HistoryManagingActivities/CustomViews/ChangeHistoryListView.java
+++ AndroidPOS/app/src/main/java/com/scoregroup/androidpos/HistoryManagingActivities/CustomViews/ChangeHistoryListView.java
@@ -2,14 +2,34 @@ package com.scoregroup.androidpos.HistoryManagingActivities.CustomViews;
 
 import android.content.Context;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.scoregroup.androidpos.R;
 
 public class ChangeHistoryListView extends LinearLayout {
 
     public ChangeHistoryListView(Context context) {
         super(context);
+        initialize();
+    }
+    private Context context;
+    private TextView keyCode, dateTime,price;
+    public void initialize(){
+        context=getContext();
+        inflate(context, R.layout.view_history_list,this);
+        keyCode =this.findViewById(R.id.name);
+        dateTime =this.findViewById(R.id.amount);
+        price=this.findViewById(R.id.price);
+    }
+    public void setData(String keyCode,String dateTime,int price){
+        this.keyCode.setText(keyCode);
+        this.dateTime.setText(dateTime);
+        this.price.setText(price);
     }
 
-    public void Initialize(){
-
+    @Override
+    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+        super.onLayout(b,i,i1,i2,i3);
     }
 }
+
--- AndroidPOS/app/src/main/java/com/scoregroup/androidpos/HistoryManagingActivities/CustomViews/HistoryItemView.java
+++ AndroidPOS/app/src/main/java/com/scoregroup/androidpos/HistoryManagingActivities/CustomViews/HistoryItemView.java
@@ -2,19 +2,28 @@ package com.scoregroup.androidpos.HistoryManagingActivities.CustomViews;
 
 import android.content.Context;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.scoregroup.androidpos.R;
 
 public class HistoryItemView extends LinearLayout {
     private Context context;
+    private TextView name,amount,price;
     public HistoryItemView(Context context) {
         super(context);
-        Initialize();
+        initialize();
     }
-    public void Initialize(){
+    public void initialize(){
         context=getContext();
         inflate(context, R.layout.view_history_item_view,this);
-        
+        name=this.findViewById(R.id.name);
+        amount=this.findViewById(R.id.amount);
+        price=this.findViewById(R.id.price);
+    }
+    public void setData(String name,int amount,int pricePerItem){
+        this.name.setText(name);
+        this.amount.setText(""+amount);
+        price.setText(amount*pricePerItem);
     }
 
     @Override
