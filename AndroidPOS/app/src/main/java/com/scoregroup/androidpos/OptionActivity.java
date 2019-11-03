7bc7cd48a80c69ce0e4cd9178e8aec5d071e17bd
JUMINJONG <wnalsals12@naver.com>
2019-11-04 00:53:34 +0900

    통계/옵션 액티비티 구현

--- AndroidPOS/app/src/main/java/com/scoregroup/androidpos/OptionActivity.java
+++ AndroidPOS/app/src/main/java/com/scoregroup/androidpos/OptionActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class OptionActivity extends AppCompatActivity {
     Button buttons[] = new Button[2];
@@ -16,6 +17,13 @@ public class OptionActivity extends AppCompatActivity {
 
         buttons[0] = findViewById(R.id.Confirm);
         buttons[1] = findViewById(R.id.ExitButton);
+
+        buttons[0].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Confirm!", Toast.LENGTH_LONG).show();
+            }
+        });
 
         buttons[1].setOnClickListener(new View.OnClickListener() {
             @Override
